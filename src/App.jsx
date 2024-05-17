6c89b8b9b4430cb9fa74b48f10d1f87b474e9159
KusalDemo <kusaldemo2022@gmail.com>
2024-05-17 12:41:02 +0530

    Adding src/

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ResponsiveAppBar from "./component/AppBar/appBar.jsx";
+
+function App() {
+
+  return (
+   <div>
+     <ResponsiveAppBar />
+   </div>
+  )
+}
+
+export default App
